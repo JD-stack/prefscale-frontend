352a559f2e230e3df9b419261cb3de2670da3ece
amay243551 <amaydevshatwar28@gmail.com>
2026-01-25 15:16:55 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://prefscale-backend.onrender.com",
+  baseURL: "https://prefscale-backend-2egb.onrender.com",
 });
 
 export default api;
