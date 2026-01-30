518daac4d76c01376cf261ba2b9fe05d7a267f32
amay243551 <amaydevshatwar28@gmail.com>
2026-01-30 13:05:54 +0530

    Add Blog link to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,6 +18,8 @@ export default function Navbar({ user, setUser }) {
       <div className="flex items-center gap-6 text-gray-700">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+
         {user ? (
           <>
             <span className="text-sm text-gray-600">
